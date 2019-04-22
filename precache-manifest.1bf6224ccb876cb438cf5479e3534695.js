f1cc07e8fcb3b0dd59a238fc506f2d5f760f18d3
Ashhal S <ashhalsyed@gmail.ca>
2019-04-22 01:38:02 -0400

    Updates

---
+++ precache-manifest.1bf6224ccb876cb438cf5479e3534695.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87ffa7743161e0dd332d",
+    "url": "/Calc-Cards/static/css/main.77faef1e.chunk.css"
+  },
+  {
+    "revision": "87ffa7743161e0dd332d",
+    "url": "/Calc-Cards/static/js/main.ea82186b.chunk.js"
+  },
+  {
+    "revision": "405ef092458f2a21fba4",
+    "url": "/Calc-Cards/static/js/runtime~main.04ccc6f1.js"
+  },
+  {
+    "revision": "3867f7bab7143a2505ac",
+    "url": "/Calc-Cards/static/js/2.db9ab169.chunk.js"
+  },
+  {
+    "revision": "c36c3d71979e193e430add88ce7412e1",
+    "url": "/Calc-Cards/index.html"
+  }
+];
