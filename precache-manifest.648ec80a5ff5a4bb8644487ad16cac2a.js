7e9edb43f310a6c4a67faa80ee06459f0093dfad
ashhalsyed <ashhalsyed@gmail.com>
2020-07-23 11:56:27 -0400

    Updates

---
+++ precache-manifest.648ec80a5ff5a4bb8644487ad16cac2a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bb17552d6ba5da88f0540a6e5ea8ec",
+    "url": "/Calc-Cards/index.html"
+  },
+  {
+    "revision": "67ade1a35fb4c5351c3e",
+    "url": "/Calc-Cards/static/css/main.c1052375.chunk.css"
+  },
+  {
+    "revision": "5533c0c91cb59b49529e",
+    "url": "/Calc-Cards/static/js/2.902f470a.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/Calc-Cards/static/js/2.902f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ade1a35fb4c5351c3e",
+    "url": "/Calc-Cards/static/js/main.a51bb5f8.chunk.js"
+  },
+  {
+    "revision": "c13a35fc7521246e6699",
+    "url": "/Calc-Cards/static/js/runtime-main.a5f051c2.js"
+  },
+  {
+    "revision": "741b067150893d8a7cc568d1c56d5b6d",
+    "url": "/Calc-Cards/static/media/Logo Black.741b0671.png"
+  },
+  {
+    "revision": "d088e79bdaf704192a49f7355d6047df",
+    "url": "/Calc-Cards/static/media/calcCardsLogo.d088e79b.svg"
+  },
+  {
+    "revision": "c15b3700c6004a01d029483a7b924015",
+    "url": "/Calc-Cards/static/media/circle.c15b3700.svg"
+  },
+  {
+    "revision": "5418f96be218aa1e1651c1f3b2ad9514",
+    "url": "/Calc-Cards/static/media/cube.5418f96b.svg"
+  },
+  {
+    "revision": "f45aac98df1d101029fa118d05cd6ee6",
+    "url": "/Calc-Cards/static/media/cylinder.f45aac98.svg"
+  },
+  {
+    "revision": "c2807fbbdc28ff540c069fdaf39a7500",
+    "url": "/Calc-Cards/static/media/line.c2807fbb.svg"
+  },
+  {
+    "revision": "ef1273cffebf5ee91bdff424377869aa",
+    "url": "/Calc-Cards/static/media/noImage.ef1273cf.svg"
+  },
+  {
+    "revision": "a6f1f3d8bc0ed409a05336a6cc273ecc",
+    "url": "/Calc-Cards/static/media/rectangle.a6f1f3d8.svg"
+  },
+  {
+    "revision": "df724f9783fa9d038ad6bb0bc3a53590",
+    "url": "/Calc-Cards/static/media/sphere.df724f97.svg"
+  },
+  {
+    "revision": "cee5bade9c00c38e7114c1017ff30478",
+    "url": "/Calc-Cards/static/media/square.cee5bade.svg"
+  },
+  {
+    "revision": "6366578f630abed34b41972d2b3f890a",
+    "url": "/Calc-Cards/static/media/triangle.6366578f.svg"
+  }
+]);
